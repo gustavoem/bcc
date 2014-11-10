af4da225a571cbaf927a4dd459a698aba5bcf8e3
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2014-11-09 22:44:19 -0200

    improovei o miniep5

--- 4semestre/labprog2/miniep5/ep5.py
+++ 4semestre/labprog2/miniep5/ep5.py
@@ -6,7 +6,9 @@ tokens = [
    'PLUS',
    'MINUS',
    'TIMES',
-   'DIVIDE']
+   'DIVIDE',
+   'VAR',
+   'EQUAL']
 
 # Regex para os tokens
 t_NUMBER  = r'[0-9]*\.*[0-9]+'
@@ -14,6 +16,8 @@ t_PLUS    = r'\+'
 t_MINUS   = r'-'
 t_TIMES   = r'\*'
 t_DIVIDE  = r'/'
+t_VAR     = r'[A-z]{1}([A-z]|[0-9])*'
+t_EQUAL   = r'='
 t_ignore  = ' \t\n' #ignorar tabs e espacos e \n
 
 # Regra para lidar com erros
@@ -24,21 +28,39 @@ def t_error(t):
 # instancia o analex
 lexer = lex.lex()
 
+var = dict()
 # gramatica
+def p_expr_equal(p):
+    '''expr : expr expr EQUAL'''
+    if type(p[1]) is not str:
+        print("Atribuicao invalida")
+    else:
+        var[p[1]] = p[2]
+        p[0] = p[1]
+
+def get_value(a):
+    if a in var:
+        a = var[a]
+    return a
+
 def p_expr_plus(p):
     '''expr : expr expr PLUS'''
+    p[1], p[2] = get_value(p[1]), get_value(p[2])
     p[0] = p[1] + p[2]
 
 def p_expr_minus(p):
     '''expr : expr expr MINUS'''
+    p[1], p[2] = get_value(p[1]), get_value(p[2])
     p[0] = p[1] - p[2]
 
 def p_expr_times(p):
     '''expr : expr expr TIMES'''
+    p[1], p[2] = get_value(p[1]), get_value(p[2])    
     p[0] = p[1] * p[2]
 
 def p_expr_divide(p):
     '''expr : expr expr DIVIDE'''
+    p[1], p[2] = get_value(p[1]), get_value(p[2])
     try:
         p[0] = p[1] / p[2]
     except ZeroDivisionError: 
@@ -52,12 +74,19 @@ def p_expr_number(p):
     else:
         p[0] = int(p[1])
 
+def p_expr_var(p):
+    '''expr : VAR'''
+    if p[1] in var:
+        p[0] = p[1]
+    else:
+        var[p[1]] = 0
+        p[0] = p[1]
+
 def p_error(p):
     print("Ops! Sintaxe invalida")
 
 # instancia o yacc. Os argumentos sao para evitar mensagens de debug
-yacc.yacc(debug = 0, write_tables = 0) 
-
+yacc.yacc(debug = 1, write_tables = 1) 
 s = 1
 while s == 1:
     try:
@@ -67,4 +96,7 @@ while s == 1:
     else:
         r = yacc.parse(line)
         if r is not None:
-            print(" " + str(r))
+            if r in var:
+                print(" " + str(var[r]))
+            else:
+                print(" " + str(r))

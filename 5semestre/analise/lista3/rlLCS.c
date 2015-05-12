f3e1ff188b85711efd1f1e889b60c0a9da7152fd
Gustavo Estrela de Matos <estrela.gustavo@hotmail.com>
2015-05-12 19:58:35 -0300

    progresso que fiz na linux

--- 5semestre/analise/lista3/rlLCS.c
+++ 5semestre/analise/lista3/rlLCS.c
@@ -10,31 +10,35 @@ int * lLCS (char * x, int n, char * y, int m)
 {
     int i, j;
     int * L = (int *) malloc (NMAX * sizeof (int));
-    int v1[NMAX];
-    int v2[NMAX];
-    
+    int u[NMAX];
+    int v[NMAX];
+    int * C1 = u;
+    int * C2 = v;
+        
     
     // ultima coluna
     // lLCS (xin, ymm) = 1 se ymm in xin; 0 c.c.
     for (i = n; i > 0 && y[m] != x[i]; i--)
-        opt[i][m] = 0;
+        C2[i] = 0;
 
     while (i > 0)
-        opt[i--][m] = 1;
+        C2[i--] = 1;
 
     for (j = m - 1; j > 0; j--)
     {
+        // C1 e a j-esima coluna da matriz opt
+        // C2 e a j-esima+1 
         // lLCS (xnn, yjm) = 1 se yjm in xnn; 0 c.c.
-        opt[n][j] = (x[n] == y[j]) || (opt[n][j+1]);
+        C1[n] = (x[n] == y[j]) || (C2[n]);
         for (i = n - 1; i > 0; i--)
         {
             if (x[i] == y[j])
-                opt[i][j] = 1 + opt[i + 1][j + 1];
+                C1[i] = 1 + C2[i + 1];
             else 
-                if (opt[i][j + 1] >= opt[i + 1][j])
-                    opt[i][j] = opt[i][j + 1];
+                if (C2[i] >= C1[i + 1])
+                    C1[i] = C2[i];
                 else
-                    opt[i][j] = opt[i + 1][j];
+                    C1[i] = opt[i + 1][j];
         }
     }
 

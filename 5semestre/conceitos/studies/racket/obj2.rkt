6d4e5ec01548cee3e741a1bf0d795940a68bb835
Gustavo Estrela <estrela.gustavo.matos@gmail.com>
2015-05-21 15:38:36 -0300

    Fiz o primeiro exemplo de objetos recursivos, usando let. Depois vou implementar com combinador Y (medo)

--- 5semestre/conceitos/studies/racket/obj2.rkt
+++ 5semestre/conceitos/studies/racket/obj2.rkt
@@ -17,23 +17,53 @@
 ; perceba que exite estado. a variável x é local para cada objeto.
 ; x está no eviroment do lambda (m)
 ;
-(define (o-constr1 x)
-  (lambda (m)
-    (case m
-      [(addX) (lambda (y) (+ x y))])))
-
-(test (let ([obj (o-constr1 20)]) (msg obj 'addX 21)) 41)
-(test (((o-constr1 10) 'addX) 1) 11)
+;(define (o-constr1 x)
+;  (lambda (m)
+;    (case m
+;      [(addX) (lambda (y) (+ x y))])))
+;
+;(test (let ([obj (o-constr1 20)]) (msg obj 'addX 21)) 41)
+;(test (((o-constr1 10) 'addX) 1) 11)
 
 ; Membros privados
 ;
-(define (o-state-2 init)
-  (let ([count init])
-    (lambda (m)
-      (case m
-        [(inc) (lambda () (set! count (+ count 1)))]
-        [(dec) (lambda () (set! count (- count 1)))]
-        [(get) (lambda () count)]))))
+;(define (o-state-2 init)
+;  (let ([count init])
+;    (lambda (m)
+;      (case m
+;        [(inc) (lambda () (set! count (+ count 1)))]
+;        [(dec) (lambda () (set! count (- count 1)))]
+;        [(get) (lambda () count)]))))
+;  (((o-state-2 10) 'get))
 
-  (((o-state-2 10) 'get))
 
+; Membros estáticos!
+;
+
+;(define (o-static-1)
+;  (let ([count 0]) ; esse count vai entrar pro env do construtor! Ou seja, pra esse 
+;                   ; count aumentar tem que fazer um let pro construtor e usá-lo para
+;                   ; todos os objetos que voce quiser instanciar
+;    (lambda (base)
+;      (begin
+;      (set! count (+ 1 count))
+;      (lambda (m)
+;        (case m
+;          [(addX) (lambda (x) (+ base x))]
+;          [(incB) (lambda () (+ 1 base))]
+;          [(getCounter) (lambda () (display count))]
+;      ))))))
+;(let ([const (o-static-1)]) (begin (((const 10) 'getCounter)) (((const 10) 'getCounter)) ))
+
+; Objetos com Auto-referencia usando let
+
+(define o-self!
+  (let ([self 'dummy])
+    (begin 
+      (set! self
+        (lambda (m)
+          (case m
+            [(add1) (lambda (x) (+ ((self 'one)) x))]
+            [(one) (lambda () 1)]))) self)))
+
+(test ((o-self! 'add1) 10) 11)

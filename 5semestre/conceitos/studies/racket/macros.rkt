a3de1f75150574c0e481fa72031ed5a682026079
Gustavo Estrela <estrela.gustavo.matos@gmail.com>
2015-05-30 20:06:49 -0300

    Exemplinho de macros

---
+++ 5semestre/conceitos/studies/racket/macros.rkt
@@ -0,0 +1,27 @@
+#lang plai
+
+
+(define-syntax my-let-1
+  (syntax-rules ()
+    [(my-let-1 (var val) body)
+     ((lambda (var) body) val)]))
+
+(test (my-let-1 (x 10) (- x 9)) 1)
+
+
+(define-syntax (my-let-3 x)
+  (syntax-case x ()
+    [(my-let-3 (var val) body)
+     #'((lambda (var) body) val)]))
+
+(test (my-let-1 (x 10) (- x 9)) 1)
+
+
+(define-syntax (my-or x) 
+  (syntax-case x () 
+    [(my-or-3) #'#f]
+    [(my-or-3 a) #'a]
+    [(my-or-3 a b ...) #'(if a a (my-or-3 b ...))]))
+
+(test (my-or #t #f #f) #t)
+(test (my-or #f) #f)

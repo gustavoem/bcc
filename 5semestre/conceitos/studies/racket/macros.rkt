4b08d382d17bf0bdc1e5c96a6068777f75fd2343
Gustavo Estrela <estrela.gustavo.matos@gmail.com>
2015-05-30 22:01:20 -0300

    macros...

--- 5semestre/conceitos/studies/racket/macros.rkt
+++ 5semestre/conceitos/studies/racket/macros.rkt
@@ -16,24 +16,27 @@
 (test (my-let-1 (x 10) (- x 9)) 1)
 
 
-(define-syntax (my-or x) 
+(define-syntax (my-or-3 x) 
   (syntax-case x () 
     [(my-or-3) #'#f]
     [(my-or-3 a) #'a]
     [(my-or-3 a b ...) #'(if a a (my-or-3 b ...))]))
 
-(test (my-or #t #f #f) #t)
-(test (my-or #f) #f)
+(test (my-or-3 #t #f #f) #t)
+(test (my-or-3 #f) #f)
 
 
 ; Protegendo a avaliacao
 ; Se duplicarmos uma avaliação na expansão de uma expressão podemos causar
 ; avaliações desnecessárias e até problemas de estado (imagine um i++)!
-(define-syntax (my-or x) 
+(define-syntax (my-or-4 x) 
   (syntax-case x () 
-    [(my-or-3) #'#f]
-    [(my-or-3 a) #'a]
-    [(my-or-3 a b ...) 
-     (let ([v a])
-     #'(if v v (my-or-3 b ...)))]))
+    [(my-or-4) #'#f]
+    [(my-or-4 a) #'a]
+    [(my-or-4 a b ...) 
+     #'(let ([v a])
+        (if v v (my-or-4 b ...)))]))
+
+(test (my-or-4 #t #f #f) #t)
+(test (my-or-4 #f) #f)
 

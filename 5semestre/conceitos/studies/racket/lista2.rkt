a0904f6caff2865f2cca2d02b9ba897e97d39183
Gustavo Estrela <estrela.gustavo.matos@gmail.com>
2015-06-27 16:41:41 -0300

    ja tirei o arquivo daqui...

--- 5semestre/conceitos/studies/racket/lista2.rkt
+++ 5semestre/conceitos/studies/racket/lista2.rkt
@@ -1,4 +1,7 @@
 #lang racket
+(let ([test (list "aqui o car funciona\n")]) (display (car test)))
+(list? (append (list) 1))
+; 1-
 (define-syntax (object/self x)
   (syntax-case x ()
     [(object [mtd-name (var) val] ...)
@@ -6,11 +9,17 @@
       #'(let ([self (lambda (msg) (error 'object/self "Objeto vazio"))])
         (begin
             (set! self
-                (let ([LC (list 1 2)])
+                (let ([LC (list)])
                     (lambda (msg)
                         (case msg
-                            [(queue) (lambda () (display LC))]
-                            [(dispara) (lambda () (0))]
+                            [(dispara) (car LC)]
+                            [(registra) (lambda (k1 k2) (begin (set! LC (append LC (list k1)))
+                                                               (k2)
+                                                               ;(display "LC depois: ")
+                                                               ;(display LC)
+                                                               ;(display (list? LC))
+                                                               ;(display "\n")
+                                                               ))]
                             [(mtd-name) (lambda (var) val)] 
                         ...))))
             self)))]))
@@ -20,4 +29,7 @@
     [soma1 (x) (+ x 1)]
     [soma2 (x) ((self 'soma1) ((self 'soma1)x))]))
 
-((obj1 'queue))
+((obj1 'registra) (lambda () (display "entra esse na lista de continuacoes")) (lambda () "wow, i'm the next!"))
+(obj1 'dispara)
+((obj1 'registra) (lambda () (display "mais um em LC")) (lambda () "wow, i'm the next!"))
+

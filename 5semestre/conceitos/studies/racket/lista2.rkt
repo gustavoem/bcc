6e747bda11ca3ea8f6125587b8d0299c1be0c332
Gustavo Estrela <estrela.gustavo.matos@gmail.com>
2015-06-29 21:49:47 -0300

    Mais um progresso na lista de conceitos... acho que vou entregar assim

--- 5semestre/conceitos/studies/racket/lista2.rkt
+++ 5semestre/conceitos/studies/racket/lista2.rkt
@@ -1,6 +1,4 @@
 #lang racket
-(let ([test (list "aqui o car funciona\n")]) (display (car test)))
-(list? (append (list) 1))
 ; 1-
 (define-syntax (object/self x)
   (syntax-case x ()
@@ -12,24 +10,39 @@
                 (let ([LC (list)])
                     (lambda (msg)
                         (case msg
-                            [(dispara) (car LC)]
+                            [(dispara) (begin ((first LC))
+                                              (set! LC (rest LC)))]
                             [(registra) (lambda (k1 k2) (begin (set! LC (append LC (list k1)))
                                                                (k2)
-                                                               ;(display "LC depois: ")
-                                                               ;(display LC)
-                                                               ;(display (list? LC))
-                                                               ;(display "\n")
                                                                ))]
                             [(mtd-name) (lambda (var) val)] 
                         ...))))
             self)))]))
 
-(define obj1
-  (object/self
-    [soma1 (x) (+ x 1)]
-    [soma2 (x) ((self 'soma1) ((self 'soma1)x))]))
 
-((obj1 'registra) (lambda () (display "entra esse na lista de continuacoes")) (lambda () "wow, i'm the next!"))
-(obj1 'dispara)
-((obj1 'registra) (lambda () (display "mais um em LC")) (lambda () "wow, i'm the next!"))
+; 2-
+(define-syntax (object/self-2 x)
+  (syntax-case x ()
+    [(object [mtd-name (var) val] ...)
+    (with-syntax ([self (datum->syntax x 'self)])
+      #'(let ([self (lambda (msg) (error 'object/self-2 "Objeto vazio"))])
+        (begin
+            (set! self
+                (let ([LC (list)])
+                      [yield (call/cc (lambda (k) ((self registra) k (self dispara))))]
+                    (lambda (msg)
+                        (case msg
+                            [(dispara) (begin ((first LC))
+                                              (set! LC (rest LC)))]
+                            [(registra) (lambda (k1 k2) (begin (set! LC (append LC (list k1)))
+                                                               (k2)
+                                                               (display "LC depois: ")
+                                                               (display LC)
+                                                               ;(display (list? LC))
+                                                               (display "\n")
+                                                               ))]
+                            [(mtd-name) (lambda (var) val)] 
+                        ...))))
+            self)))]))
+
 

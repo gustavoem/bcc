e18f093b87302e2916c289315a8828732b803c0e
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2015-09-03 11:27:11 -0500

    Now using glutMainLoop () everything seems ok (did not need to kill the program to close it)

--- tamu/1semester/441/studies/create_window.c
+++ tamu/1semester/441/studies/create_window.c
@@ -1,12 +1,18 @@
 #include <GL/glut.h>
 
+void lineSegment (void)
+{
+    return;
+}
+
+
 int main (int argc, char ** argv)
 {
     glutInit (&argc, argv);
-    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize (500, 500);
     glutInitWindowPosition (100, 100);
     glutCreateWindow ("Primeira janela");
-    while (1);
+    glutDisplayFunc (lineSegment);
+    glutMainLoop ();
     return 0;
 }

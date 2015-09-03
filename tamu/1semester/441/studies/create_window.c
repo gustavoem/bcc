b04af399de10a0bf80468823a89c9904c3c4465c
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2015-09-03 12:26:43 -0500

    Agora desenhamos uma linha preta no fundo branco

--- tamu/1semester/441/studies/create_window.c
+++ tamu/1semester/441/studies/create_window.c
@@ -3,6 +3,13 @@
 void lineSegment (void)
 {
     glClear (GL_COLOR_BUFFER_BIT);
+    
+    glColor3f (.0, .4, .2);
+    glBegin (GL_LINES);
+        glVertex2i (10, 10);
+        glVertex2i (10, 140);
+    glEnd ();
+
     glFlush ();
 }
 
@@ -10,7 +17,8 @@ void lineSegment (void)
 void init ()
 {
     glClearColor (1.0, 1.0, 1.0, .0);
-
+    glMatrixMode (GL_PROJECTION);
+    gluOrtho2D (0.0, 200.0, 0.0, 150.0);
 }
 
 
@@ -22,6 +30,7 @@ int main (int argc, char ** argv)
     glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
     glutCreateWindow ("Primeira janela");
     glutDisplayFunc (lineSegment);
+    init ();
     glutMainLoop ();
     return 0;
 }

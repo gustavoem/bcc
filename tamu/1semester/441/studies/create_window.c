d6c930e252433d7cd03fccdca4bb2d426566541e
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2015-09-03 11:58:06 -0500

    Now we set the clear color buffer and we clear the window everytime we are displaying something

--- tamu/1semester/441/studies/create_window.c
+++ tamu/1semester/441/studies/create_window.c
@@ -2,7 +2,15 @@
 
 void lineSegment (void)
 {
-    return;
+    glClear (GL_COLOR_BUFFER_BIT);
+    glFlush ();
+}
+
+
+void init ()
+{
+    glClearColor (1.0, 1.0, 1.0, .0);
+
 }
 
 
@@ -11,6 +19,7 @@ int main (int argc, char ** argv)
     glutInit (&argc, argv);
     glutInitWindowSize (500, 500);
     glutInitWindowPosition (100, 100);
+    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
     glutCreateWindow ("Primeira janela");
     glutDisplayFunc (lineSegment);
     glutMainLoop ();

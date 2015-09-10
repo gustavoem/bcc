89b01e2c5475a91e990381a58ef5e6182faab9e5
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2015-09-10 07:48:52 -0500

    OS P1 done

--- tamu/1semester/410/P1/kernel.C
+++ tamu/1semester/410/P1/kernel.C
@@ -38,7 +38,7 @@ int main()
   Console::puts("WELCOME TO MY KERNEL!\n");
   Console::puts("      ");
   Console::set_TextColor(GREEN, RED);
-  Console::puts("<NAME>\n");
+  Console::puts("Gustavo Estrela\n");
 
   /* -- LOOP FOREVER! */
   for(;;);

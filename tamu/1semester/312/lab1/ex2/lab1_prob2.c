699485d9f6a144164cd80e5f3488f539f72489fe
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2015-09-13 16:11:59 -0500

    ex2 done

--- tamu/1semester/312/lab1/ex2/lab1_prob2.c
+++ tamu/1semester/312/lab1/ex2/lab1_prob2.c
@@ -3,14 +3,7 @@
 
 int main()
 {
-    unsigned int uint;
-    double dbl;
-    long lon;
-    long long lonlon;
-    char ch;
-    float fl;
     struct timeval tmv;
-
 
     FILE *my_file_pointer;
     if ((my_file_pointer = fopen ("lab1_prob2_out.txt", "w")) == NULL)
@@ -18,11 +11,15 @@ int main()
         printf ("Error opening the file, so exiting\n");
         return 1;
     }
-
     fprintf (my_file_pointer, "The sizes of different data type for this machine and compiler are -\n");
+    fprintf (my_file_pointer, "unsigned int data type is %lu bytes or %lu bits long\n", sizeof (unsigned int), sizeof (unsigned int) * 8);
+    fprintf (my_file_pointer, "double data type is %lu bytes or %lu bits long\n", sizeof (double), sizeof (double) * 8);
+    fprintf (my_file_pointer, "long data type is %lu bytes or %lu bits long\n", sizeof (long), sizeof (long) * 8);
+    fprintf (my_file_pointer, "long long data type is %lu bytes or %lu bits long\n", sizeof (long long), sizeof (long long) * 8);
+    fprintf (my_file_pointer, "char data type is %lu bytes or %lu bits long\n", sizeof (char), sizeof (char) * 8);
+    fprintf (my_file_pointer, "float data type is %lu bytes or %lu bits long\n", sizeof (float), sizeof (float) * 8);
+    fprintf (my_file_pointer, "struct timeval data type is %lu bytes or %lu bits long\n", sizeof (tmv), sizeof (tmv) * 8);
 
-    lon = 9.223372e+18 + 101312313;
-    printf ("Valor de lon: %30lu", lon);
 
     printf ("The sizes of different data type for this machine and compiler are -\n");
     printf ("unsigned int data type is %lu bytes or %lu bits long\n", sizeof (unsigned int), sizeof (unsigned int) * 8);

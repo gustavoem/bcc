3a0f55d65a5d7bccb952cb5463497e21cd9b6e6e
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2015-09-27 17:39:36 -0500

    Implemented FramePool (i guess xD)

--- tamu/1semester/410/P2/frame_pool.C
+++ tamu/1semester/410/P2/frame_pool.C
@@ -1,5 +1,7 @@
 #include "frame_pool.H"
 
+FramePool * FramePool::head_frame = 0;
+
 FramePool::FramePool (unsigned long _base_frame_no, unsigned long _nframes, 
     unsigned long _info_frame_no)
 {
@@ -11,7 +13,7 @@ FramePool::FramePool (unsigned long _base_frame_no, unsigned long _nframes,
 
     // Sets the free_frame address
     free_frames = (unsigned long *) 
-        ((info_frame_n) * (FRAME_SIZE / LONG_INT_SIZE));
+        ((info_frame_n) * (FRAME_SIZE / LONG_SIZE));
 
     // Saves parameters
     base_n = _base_frame_no;
@@ -23,9 +25,14 @@ FramePool::FramePool (unsigned long _base_frame_no, unsigned long _nframes,
         free_frames[i] = 0;
     }
 
+    // List of frames update
+    FramePool * prev = FramePool::head_frame;
+    FramePool::head_frame = this;
+    previous_frame = prev;
+
     // Makes the info frame unavailable
-    unsigned long j = info_frame_n / LONG_INT_SIZE;
-    unsigned long mask = 1 << (info_frame_n % LONG_INT_SIZE);
+    unsigned long j = info_frame_n / LONG_SIZE;
+    unsigned long mask = 1 << (info_frame_n % LONG_SIZE);
     free_frames[j] = mask;
 }
 
@@ -34,8 +41,8 @@ unsigned long FramePool::get_frame ()
 {
     for (unsigned long i = 0; i < frames_n; i++)
     {
-        unsigned long j = i / LONG_INT_SIZE + 2;
-        unsigned long mask = 1 << (i % LONG_INT_SIZE);
+        unsigned long j = i / LONG_SIZE + 2;
+        unsigned long mask = 1 << (i % LONG_SIZE);
         if (!(free_frames[j] & mask))
         {
             // Makes frame unavailable and return frame number
@@ -51,8 +58,8 @@ void FramePool::mark_inaccessible (unsigned long _base_frame_no, unsigned long _
 {
     for (unsigned long i = _base_frame_no; i < _base_frame_no + _nframes; i++)
     {
-        unsigned long j = i / LONG_INT_SIZE + 2;
-        unsigned long mask = 1 << (i % LONG_INT_SIZE);
+        unsigned long j = i / LONG_SIZE + 2;
+        unsigned long mask = 1 << (i % LONG_SIZE);
         free_frames[j] = free_frames[j] | mask;
     }
 }
@@ -60,11 +67,23 @@ void FramePool::mark_inaccessible (unsigned long _base_frame_no, unsigned long _
 
 void FramePool::release_frame (unsigned long _fram_no)
 {
-    /*// First identify the currect frame
-    unsigned long * frame_pool = 0x0;
+    // First identify the currect frame
+    FramePool * frame_pool = FramePool::head_frame;
 
-    // Now free the frame
-    unsigned long j = _fram_no / LONG_INT_SIZE + 2;
-    unsigned long mask = ~(1 << (_fram_no % LONG_INT_SIZE));
-    free_frames[j] = free_frames[j] & mask;*/
+    while (frame_pool != 0 && 
+        (frame_pool->base_n > _fram_no || 
+        ((frame_pool->base_n + frame_pool->frames_n) < _fram_no)))
+        frame_pool = frame_pool->previous_frame;
+
+
+    // I think I should do smth more alarmous here but still not sure how. This happens
+    // when someone tries to release a frame with an index out of the possible range
+    if (frame_pool == 0) return; 
+
+    // Now, assuming that we found the frame_pool, free the frame
+    unsigned long info_n = frame_pool->info_frame_n;
+    unsigned long * free_frames = (unsigned long *) (info_n * (FRAME_SIZE / LONG_SIZE));
+    unsigned long j = _fram_no / LONG_SIZE + 2;
+    unsigned long mask = ~(1 << (_fram_no % LONG_SIZE));
+    free_frames[j] = free_frames[j] & mask;
 }

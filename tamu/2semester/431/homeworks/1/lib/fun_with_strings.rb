124e3b377090bb91e0d665c0e3e949ca02f634fc
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2016-02-03 10:25:49 -0600

    Finished count words

--- tamu/2semester/431/homeworks/1/lib/fun_with_strings.rb
+++ tamu/2semester/431/homeworks/1/lib/fun_with_strings.rb
@@ -3,12 +3,24 @@ module FunWithStrings
     s = self.downcase.gsub(/\W+/, "")
     s == s.reverse
   end
+
   def count_words
-    # your code here
+    v = self.downcase.split(/\W+/).reject{|s| s.empty?}
+    hash = {}
+    v.each{|x| 
+        if hash.has_key?(x)
+            hash[x] = hash[x] + 1
+        else
+            hash[x] = 1
+        end
+    }
+    hash
   end
+
   def anagram_groups
     # your code here
   end
+
 end
 
 # make all the above functions available as instance methods on Strings:

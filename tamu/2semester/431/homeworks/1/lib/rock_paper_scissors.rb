b7db703a00efc18d983f57c3e6bec4fd3459d4ec
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2016-02-04 17:01:58 -0600

    431 rock paper scissor has now a winner

--- tamu/2semester/431/homeworks/1/lib/rock_paper_scissors.rb
+++ tamu/2semester/431/homeworks/1/lib/rock_paper_scissors.rb
@@ -4,7 +4,19 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    winner = player1
+    strength = ['R', 'S', 'P'] 
+    begin
+        z3_player1 = strength.find_index(player1[1])
+        z3_player2 = strength.find_index(player2[1])
+        # we only change winner if he lost
+        if (z3_player1 == (z3_player2 + 1) % 3)
+            winner = player2
+        end
+    rescue
+        raise NoSuchStrategyError.new("Strategy must be one of R,P,S")
+    end
+    winner
   end
 
   def self.tournament_winner(tournament)

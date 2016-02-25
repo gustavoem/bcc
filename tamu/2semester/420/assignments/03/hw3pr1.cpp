3aa76a8f6cbda4bbd45f79e30d3a46d2415f0e01
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2016-02-25 16:02:24 -0600

    progress in 420 hw3

--- tamu/2semester/420/assignments/03/hw3pr1.cpp
+++ tamu/2semester/420/assignments/03/hw3pr1.cpp
@@ -1,47 +1,72 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 class Cryptomathic 
 {
-    // The following strings represent the cryptomathic problem
-    //
-    //   term1
-    // + term2
-    // _______
-    //     sum
-    //
-    string term1;
-    string term2;
-    string sum;
+    private:
+
+        // The following strings represent the cryptomathic problem
+        //
+        //   term1
+        // + term2
+        // _______
+        //     sum
+        //
+        string term1;
+        string term2;
+        string sum;
+
+
+        // Represents the global domain for each variable, i.e this is responsible for
+        // keeping track of numbers that haven't been used yet. Useful for checking 
+        // that all letters have different values assigned
+        //
+        vector<unsigned int> global_domain;
+
+
+        // assignments[i] = C iif we assigned value i to letter C
+        //
+        vector<char> assignments;
+
+
+        // Vector with all the different letters. This vector is used to order the 
+        // assignments in our search for a solution
+        //
+        vector<char> letters;
     
 
-    // Represents the global domain for each variable, i.e this is responsible for
-    // keeping track of numbers that haven't been used yet. Useful for checking 
-    // that all letters have different values assigned
-    //
-    vector<unsigned int> global_domain;
+        // Store individual restrictions to letters
+        //       
+        map<char, vector<int> > individual_restrictions; 
+ 
+        // Make simple inferentios to restrict domain for some letters
+        //
+        void restrict_domain ()
+        {
+            // leading letter in the result can't be zero
+            individual_restrictions[sum[sum.length () - 1]].push_back (0);
+        }
 
 
-    // assignments[i] = C iif we assigned value i to letter C
-    //
-    vector<char> assignments;
+    public:
+
+        // Default Constructor
+        //
+        Cryptomathic (string string1, string string2, string string3)
+        {
+            term1 = string1;
+            term2 = string2;
+            sum = string3;
+        }
 
 
-    // Vector with all the different letters
-    //
-    vector<char> letters;
-
-
-    // Default Constructor
-    //
-    Cryptomathic (string string1, string string2, string string3)
-    {
-        term1 = string1;
-        term2 = string2;
-        sum = string3;
-    }
+        vector<char> * solve ()
+        {
+            return NULL;
+        }
 };
 
 int main ()
@@ -53,4 +78,7 @@ int main ()
     cin >> string1;
     cin >> string2;
     cin >> string3; 
+
+    Cryptomathic puzzle (string1, string2, string3);
+    vector<char> * solution = puzzle.solve ();
 }

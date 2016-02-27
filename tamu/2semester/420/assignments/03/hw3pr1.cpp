db7508efb26d2adaa8f65941ca8d38c49327102e
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2016-02-27 15:12:47 -0600

    Implemented consistency verification and assignment

--- tamu/2semester/420/assignments/03/hw3pr1.cpp
+++ tamu/2semester/420/assignments/03/hw3pr1.cpp
@@ -5,6 +5,7 @@
 #include <set>
 
 using namespace std;
+
 class Cryptomathic 
 {
     private:
@@ -25,10 +26,10 @@ class Cryptomathic
         // keeping track of numbers that haven't been used yet. Useful for checking 
         // that all letters have different values assigned
         //
-        vector<unsigned int> global_domain;
+        vector<bool> global_domain;
 
 
-        // assignments[i] = C iif we assigned value i to letter C
+        // Maps letters to values
         //
         map<char, int> assignments;
 
@@ -50,6 +51,8 @@ class Cryptomathic
         {
             // leading letter in the result can't be zero
             individual_restrictions[sum[sum.length () - 1]].insert (0);
+            individual_restrictions[term1[term1.length () - 1]].insert (0);
+            individual_restrictions[term2[term2.length () - 1]].insert (0);
 
             // if first two are equal then first number of sum is even
             if (term1[0] == term2[0])
@@ -92,65 +95,14 @@ class Cryptomathic
         }
 
         
-        // Verifies if variable assignments broken any rule
-        //
-        bool isConsistant ()
-        {
-            int max_index = max (max (string1.length (), string2.length ()), sum.length());
-            for (i = 0; i < max_index; i++)
-            {
-                // There is no reason to verify consistency on index i if one of the 
-                // variables is not defined for this index. Since we can't say anything
-                // about this index we can't say anything about greater indexes too
-                // so we can say it's consistant
-                if (!(isDefined (string1, i) && isDefined (string2, i) && isDefined (sum, i)))
-                    continue;
-
-                // verifies if there is carry
-                int carry = 0;
-                if (i > 0)
-                {
-                    if (isDefined (string1, i - 1) && isDefined (string2, i - 1))
-                    {
-                        int s1_digit = getValue (string1, i - 1);
-                        int s2_digit = getValue (string2, i - 1);
-                        int sum_digit = getValue ()
-
-                        if (s1_digit + s2_digit > 9)
-                            carry = 1;
-                    }
-                    else
-                        carry = -1;
-
-                }
-               
-                // we are assuming that these numbers are defined 
-                int s1_digit = 0;
-                int s2_digit = 0;
-                s1_digit = getValue (string1, i);
-                s2_digit = assignments (string2, i);
-                sum_digit = assignments (sum, i);
-
-                if (carry == -1)
-                    if (s1_digit + s2_digit  == sum_digit || 
-                        s1_digit + s2_digit + 1 == sum_digit)
-                        continue;
-                    else
-                        return false;
-                else
-                    if (s1_digit + s2_digit + carry == sum_digit)
-                        continue;
-                    else
-                        return false;
-            }
-        }
+        
 
         
         // Returns the value of i-th charachter of s. 
         // If i >= s.length returns 0
         // Returns -1 if characther not defined
         //
-        int getValue (string s, index i)
+        int getValue (string s, unsigned int i)
         {
             if (i < s.length ())
                 if (assignments.find (s[i]) != assignments.end ())
@@ -165,7 +117,7 @@ class Cryptomathic
         // Verifies if a character from s at index i is defined. If i >= s.length returns
         // true
         //
-        bool isDefined (string s, index i)
+        bool isDefined (string s, unsigned int i)
         {
             if (i < s.length () && assignments.find (s[i]) != assignments.end ())
                 return false;
@@ -182,19 +134,105 @@ class Cryptomathic
             term1 = string1;
             term2 = string2;
             sum = string3;
+                
+            for (unsigned int i = 0; i < 10; i++)
+                global_domain.push_back (true);
 
             restrict_domain ();
             // reorder_var ();
         }
 
-
-        vector<char> * solve ()
+        // Verifies if variable assignments broken any rule
+        //
+        bool isConsistant ()
         {
+            unsigned int max_index = max (max (term1.length (), term2.length ()), sum.length());
+            for (unsigned int i = 0; i < max_index; i++)
+            {
+                // There is no reason to verify consistency on index i if one of the 
+                // variables is not defined for this index. 
+                if (!(isDefined (term1, i) && isDefined (term2, i) && isDefined (sum, i)))
+                    continue;
+
+                // verifies if there is carry. If there is assign it to carry
+                // if not possible to determine assign -1 to carry
+                int carry = -1;
+                if (i > 0)
+                {
+                    if (isDefined (term1, i - 1) && isDefined (term2, i - 1))
+                    {
+                        int s1_digit = getValue (term1, i - 1);
+                        int s2_digit = getValue (term2, i - 1);
+
+                        if (s1_digit + s2_digit > 9)
+                            carry = 1;
+ 
+                        // We are ignoring some cases where we could already tell that
+                        // there is some inconsistency. But they are going to be
+                        // recognized in further iterations when there are more letters
+                        // defined
+
+                        if (s1_digit + s2_digit < 8)
+                            carry = 0;
+                    }
+                    else
+                        carry = -1;
+
+                }
+                else
+                {
+                    carry = 0; // i = 0
+                }
+               
+                // we are assuming that these numbers are defined 
+                int s1_digit = getValue (term1, i);
+                int s2_digit = getValue (term2, i);
+                int sum_digit = getValue (sum, i);
+
+                if (carry == -1)
+                    if (s1_digit + s2_digit  == sum_digit || 
+                        s1_digit + s2_digit + 1 == sum_digit)
+                        continue;
+                    else
+                        return false;
+                else
+                    if (s1_digit + s2_digit + carry == sum_digit)
+                        continue;
+                    else
+                        return false;
+            }
+            return true;
+        }       
+
+        
+        // Assigns value to a letter c
+        // Returns false if led the puzzle to be inconsistent
+        //
+        bool assign (char c, int value)
+        {
+            // you can't reassign variables 
+            if (assignments.find (c) != assignments.end ())
+                return false;
+               
+            // you can't assign to c restricted values
+            if (individual_restrictions[c].find (value) != 
+                    individual_restrictions[c].end ())
+                return false;
             
-            return NULL;
+            // can't assign an already used value
+            if (global_domain[value] == false)
+                return false;
+
+
+            // update attributes
+            assignments[c] = value;
+            global_domain[value] = false;
+
+            if (isConsistant ())
+                return true;
+            else
+                return false;
         }
-
-
 };
 
 int main ()
@@ -208,5 +246,5 @@ int main ()
     cin >> string3; 
 
     Cryptomathic puzzle (string1, string2, string3);
-    vector<char> * solution = puzzle.solve ();
+    cout << puzzle.assign ('A', 1) << endl;
 }

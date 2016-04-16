74e0e0c818c424b3014a89a27042d26e5ee15aa1
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2016-04-16 15:18:00 -0500

    Started 420 project

---
+++ tamu/2semester/420/project/ToffoliGate.h
@@ -0,0 +1,28 @@
+// Author: Gustavo Estrela de Matos
+// Date: April 2016
+//
+//
+
+#ifndef TOFFOLI_GATE_H_
+#define TOFFOLI_GATE_H_
+
+class ToffoliGate
+{
+    
+    private:
+
+
+    public:
+        
+        // Default constructor
+        //
+        ToffoliGate ();
+
+
+        // Default destructor
+        //
+        virtual ~ToffoliGate ();
+
+};
+
+#endif

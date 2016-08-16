99df12c8536aaf1b3bcdac8e76557d06edabe450
Gustavo Estrela <estrela.gustavo.matos@gmail.com>
2016-08-16 14:53:49 -0300

    Updated style (nothing really changed, astyle is not cool)

--- 6semestre/328/C/traco.c
+++ 6semestre/328/C/traco.c
@@ -4,12 +4,12 @@
 // SEDGEWICK OU ROBERTS, FORAM DESENVOLVIDAS POR MIM.  DECLARO
 // TAMBEM QUE SOU RESPONSAVEL POR TODAS AS COPIAS DESTE PROGRAMA
 // E QUE NAO DISTRIBUI NEM FACILITEI A DISTRIBUICAO DE COPIAS.
-// 
+//
 // Autor:      Gustavo Estrela
 // Numero USP: 8536051
 // Sigla:      GUSTAVOE
 // Data:       2016-08-17
-// 
+//
 // Este arquivo faz parte da tarefa C
 // da disciplina MAC0328 e tem como objetivo implementar e testar uma
 // variante da função DFS de que mostra o traço do algoritmo.
@@ -30,14 +30,14 @@ int count;
 int *pre;
 
 /* Protótipos de funções */
-void DIGRAPHdfs (Digraph G); 
+void DIGRAPHdfs (Digraph G);
 static void dfsR (Digraph G, Vertex v);
 
 /* A função DIGRAPHdfs() visita todos os vértices e todos os arcos do
 digrafo G. A função atribui um número de ordem pre[x] a cada vértice x:
 o k-ésimo vértice descoberto recebe número de ordem k.  (Código
 inspirado no programa 18.3 de Sedgewick.) */
-void DIGRAPHdfs (Digraph G) { 
+void DIGRAPHdfs (Digraph G) {
     Vertex v;
     pre = malloc (G->V * sizeof (int));
     count = 0;
@@ -45,13 +45,14 @@ void DIGRAPHdfs (Digraph G) {
         pre[v] = -1;
     for (v = 0; v < G->V; v++)
         if (pre[v] == -1) {
-            printf ("\n\n");
+            printf ("\n");
             dfsR (G, v);
+            printf ("\n");
         }
 }
 
 /* Seja U o conjunto dos vértices u tais que pre[u] >= 0. Nesse
-ambiente, para cada vértice x acessível a partir de v por um caminho 
+ambiente, para cada vértice x acessível a partir de v por um caminho
 que não usa vértices de U, a função dfsR() atribui um número positivo a
 pre[x]:  se x é o k-ésimo vértice descoberto, pre[x] recebe conta+k.
 (Código inspirado no programa 18.1 de Sedgewick.) */
@@ -60,23 +61,23 @@ static void dfsR (Digraph G, Vertex v) {
     link l = G->adj[v];
     int i;
     printf ("dfsR (G, %d)", v);
-    pre[v] = count++; 
+    pre[v] = count++;
     depth++;
     while (l != NULL) {
         w = l->w;
         printf ("\n");
-        for (i = 0; i < depth; i++) 
+        for (i = 0; i < depth; i++)
             printf ("  ");
         printf ("%d-%d ", v, w);
         if (pre[w] == -1)
-            dfsR (G, w); 
+            dfsR (G, w);
         l = l->next;
     }
     depth--;
 }
 
-/* Esta função recebe como parametros, em argv, V e A, e gera um 
-digrafo aleatório com V vértices e A arestas. Além disso a função 
+/* Esta função recebe como parametros, em argv, V e A, e gera um
+digrafo aleatório com V vértices e A arestas. Além disso a função
 aplica a função DIGRAPGdfs () no grafo gerado. */
 int main (int argc, char **argv) {
     int V, A;
@@ -86,9 +87,6 @@ int main (int argc, char **argv) {
     V = atoi (argv[1]);
     A = atof (argv[2]);
     G = DIGRAPHrand1 (V, A);
-    if (G->V < 10 && G->A < 21) {
-        DIGRAPHshow (G);
-    }
     DIGRAPHdfs (G);
     DIGRAPHdestroy (G);
     free (pre);

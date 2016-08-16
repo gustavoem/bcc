d37557691815078eb852ac0a9548660bd6e89ec8
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2016-08-16 09:39:54 -0300

    Fixed dfsR identation

--- 6semestre/328/C/traco.c
+++ 6semestre/328/C/traco.c
@@ -30,8 +30,8 @@ int count;
 int *pre;
 
 /* A função DIGRAPHdfs() visita todos os vértices e todos os arcos do
-digrafo G. A função atribui um número de ordem pre[x] a cada vértice
-x: o k-ésimo vértice descoberto recebe número de ordem k.  (Código
+digrafo G. A função atribui um número de ordem pre[x] a cada vértice x:
+o k-ésimo vértice descoberto recebe número de ordem k.  (Código
 inspirado no programa 18.3 de Sedgewick.) */
 void DIGRAPHdfs (Digraph G) { 
    Vertex v;
@@ -43,11 +43,17 @@ void DIGRAPHdfs (Digraph G) {
          dfsR( G, v); /* começa nova etapa */
 }
 
-/* Seja U o conjunto dos vértices u tais que pre[u] >= 0. Nesse ambiente, para cada vértice x acessível a partir de v por um caminho que não usa vértices de U, a função dfsR() atribui um número positivo a pre[x]:  se x é o k-ésimo vértice descoberto, pre[x] recebe conta+k.  (Código inspirado no programa 18.1 de Sedgewick.) */
-static void dfsR( Digraph G, Vertex v) { 
+/* Seja U o conjunto dos vértices u tais que pre[u] >= 0. Nesse
+ambiente, para cada vértice x acessível a partir de v por um caminho 
+que não usa vértices de U, a função dfsR() atribui um número positivo a
+pre[x]:  se x é o k-ésimo vértice descoberto, pre[x] recebe conta+k.
+(Código inspirado no programa 18.1 de Sedgewick.) */
+static void dfsR (Digraph G, Vertex v) {
    Vertex w;
    pre[v] = conta++; 
    for (w = 0; w < G->V; w++)
       if (G->adj[v][w] != 0 && pre[w] == -1)
          dfsR( G, w); 
 }
+
+

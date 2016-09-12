ac902cf9d134fa8487408ba5e914c697cbfd4067
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2016-09-12 13:25:57 -0300

    Implemented Kosaraju-Sharir

--- 6semestre/328/F/compsfortes.c
+++ 6semestre/328/F/compsfortes.c
@@ -26,26 +26,18 @@ então k grafos aleatórios com V vértices e
 TODO: terminar essa descrição */
 int main (int argc, char **argv) {
     int V, A, k, i;
-    Digraph G, GR;
+    Digraph G;
     if (argc != 3)
         return 0;
     V = atoi (argv[1]);
     k = atof (argv[2]);
 
-    G = DIGRAPHrand1 (V, V);  
-    GR = DIGRAPHreverse (G); 
+    G = DIGRAPHrand1 (V, V);
     DIGRAPHshow (G);
-    DIGRAPHshow (GR);
-    DIGRAPHdfs (G);
-    
+    DIGRAPHscKS (G);
     printf ("\n");
     for (i = 0; i < G->V; i++)
-        printf ("%d ", G->pre[i]);
-    printf ("\n");
-    for (i = 0; i < G->V; i++)
-        printf ("%d ", G->pos[i]);
-     
-
+        printf ("%d ", G->sc[i]);
     printf ("\n");
     return 0;
 }

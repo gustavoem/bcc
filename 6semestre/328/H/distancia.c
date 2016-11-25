69503461af8db74ac8cb7d3c4cf73152c605c45a
Gustavo Estrela <estrela.gustavo.matos@gmail.com>
2016-11-25 14:22:10 -0200

    Started graphs homework H

---
+++ 6semestre/328/H/distancia.c
@@ -0,0 +1,30 @@
+/* DECLARO QUE SOU O UNICO AUTOR E RESPONSAVEL POR ESTE PROGRAMA.
+// TODAS AS PARTES DO PROGRAMA, EXCETO AS QUE FORAM FORNECIDAS
+// PELO PROFESSOR OU COPIADAS DO LIVRO OU DAS BIBLIOTECAS DE
+// SEDGEWICK OU ROBERTS, FORAM DESENVOLVIDAS POR MIM.  DECLARO
+// TAMBEM QUE SOU RESPONSAVEL POR TODAS AS COPIAS DESTE PROGRAMA
+// E QUE NAO DISTRIBUI NEM FACILITEI A DISTRIBUICAO DE COPIAS.
+//
+// Autor:      Gustavo Estrela
+// Numero USP: 8536051
+// Sigla:      GUSTAVOE
+// Data:       2016-11-25
+//
+// Este arquivo faz parte da tarefa G
+// TODO: escrever sobre o que essa tarefa é
+//
+////////////////////////////////////////////////////////////// */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "wDIGRAPHlists.h"
+
+/* Definições */
+#define Graph Digraph
+
+/* Protótipo das funções */
+
+/* TODO: escrever sobre essa função */
+int main (int argc, char **argv) {
+    return 0;
+}
